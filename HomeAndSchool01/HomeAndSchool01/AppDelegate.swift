c9b445ce783fc65f4f08f406dc5c24792c80d5df
ljm <ljm@icloud.com>
2016-12-22 19:32:54 +0800

    初始化登录界面（appdelegate）

--- HomeAndSchool01/HomeAndSchool01/AppDelegate.swift
+++ HomeAndSchool01/HomeAndSchool01/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        //初始化登录界面
+        let loginVC = HSLoginController()
+        window?.rootViewController = loginVC
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ HomeAndSchool01/HomeAndSchool01/Classes/Login/HSLoginController.swift
@@ -0,0 +1,41 @@
+//
+//  HSLoginController.swift
+//  HomeAndSchool01
+//
+//  Created by ljm on 2016/12/22.
+//  Copyright © 2016年 ljm. All rights reserved.
+//
+
+import UIKit
+let WIDTH = UIScreen.main.bounds.size.width
+let HEIGHT = UIScreen.main.bounds.size.height
+
+class HSLoginController: UIViewController {
+    
+    var loginBtn:UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        addLoginBtn()
+    }
+    func addLoginBtn() {
+        loginBtn = UIButton(type: .custom)
+        loginBtn.frame = CGRect(x: 0, y: 0, width: WIDTH*0.3, height: HEIGHT*0.2)
+        loginBtn.center = CGPoint(x: WIDTH/2, y: HEIGHT/2)
+        loginBtn.setTitle("登录", for: .normal)
+        loginBtn.addTarget(self, action: #selector(loginBtnClicked), for: .touchUpInside)
+        view.addSubview(loginBtn)
+    }
+    
+    ///登录事件
+    func loginBtnClicked() {
+        UIApplication.shared.keyWindow?.rootViewController = HSTabBarController()
+        print("登录")
+    }
+    deinit {
+        print("HSLoginController消失了")
+    }
+    
+}
